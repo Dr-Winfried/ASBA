3f6ebd62dc1cb14c6b086f3cdf4b530cca27a3b8
Winfried Teschers <winfried.teschers@t-online.de>
2018-02-14 22:42:36 +0100

    Index überarbeitet
    Makros für Indexeinträge; Seiten mit Definitionen fett

--- LaTeX/erzeugeIndizes.bat
+++ LaTeX/erzeugeIndizes.bat
@@ -1,8 +1,9 @@
 @echo off
-echo %0 %*
-perl "C:\Program Files\MiKTeX 2.9\scripts\splitindex\splitindex.pl" ASBA
-makeglossaries.exe ASBA
-call einfuegen.bat ASBA-idx.ind 1 "  \insertidx"
-call einfuegen.bat ASBA-sym.ind 1 "  \insertsym"
-call einfuegen.bat ASBA.gls     2    \insertglo
-pause
+cd %~dp0
+cd
+rem "C:\Strawberry\perl\bin\perl.exe" "C:\Program Files\MiKTeX 2.9\scripts\splitindex\splitindex.pl"  ASBA
+                                  "C:\Program Files\MiKTeX 2.9\miktex\bin\x64\makeglossaries.exe" ASBA
+rem call einfuegen.bat ASBA-idx.ind 1 "  \insertidx"
+rem call einfuegen.bat ASBA-sym.ind 1 "  \insertsym"
+    call einfuegen.bat ASBA.gls     2    \insertglo
+if "%2" == "" pause
