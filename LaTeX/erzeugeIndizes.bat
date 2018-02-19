4ce8f4e84b22f841689b72aea239e626fdea9b44
Winfried Teschers <winfried.teschers@t-online.de>
2018-02-19 22:57:29 +0100

    Index, Symbolverzeichnis und Glossar überarbeitet
    Größere Überarbeitung. Noch nicht fertiggestellt. Verweise müssen noch
    vervollständigt werden. Außerdem fehlt die Markierung von Seiten mit der
    jeweiligen Definition noch.
    
    Sonstige Entwicklung steht bei der Definition von Beweisen. Nach
    Fertigstellung von Index, Symbolverzeichnis und Glosar muss als nächstes
    zur Motivation ein Beispiel ausgearbeitet werden.
    
    Die Ausarbeitung zur Aussagenlogik kann wahrscheinlich zum größten Teil
    entfallen. Für die Tabelle wäre es sehr schade.

--- LaTeX/erzeugeIndizes.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd %~dp0
-cd
-rem "C:\Strawberry\perl\bin\perl.exe" "C:\Program Files\MiKTeX 2.9\scripts\splitindex\splitindex.pl"  ASBA
-                                  "C:\Program Files\MiKTeX 2.9\miktex\bin\x64\makeglossaries.exe" ASBA
-rem call einfuegen.bat ASBA-idx.ind 1 "  \insertidx"
-rem call einfuegen.bat ASBA-sym.ind 1 "  \insertsym"
-    call einfuegen.bat ASBA.gls     2    \insertglo
-if "%2" == "" pause
